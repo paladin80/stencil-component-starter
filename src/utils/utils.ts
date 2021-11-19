8e6e685041463b1129ee4859c443394fd4caffb3
paladin80 <paladin80@gmail.com>
2021-11-19 15:19:21 +0100

    problematic code for tests

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,7 @@
 export function format(first: string, middle: string, last: string): string {
+  if (1 == 2) {
+    first = middle;
+  }
+  '1'.d();
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
